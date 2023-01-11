d6309cdc7ab9ddba8edd5935da1a22bc35b90fe7
Jorge Harbes <jharbes@hotmail.com>
2023-01-11 20:56:54 -0300

    Create 00-funcoesEmSql.sql

---
+++ 00-funcoesEmSql.sql
@@ -0,0 +1,24 @@
+/*
+
+FUNCOES EM SQL
+
+count() - soma os registros definidos pela tabela ou pela consulta definida
+
+max() - retorna o maior valor entre uma tabela ou referente a consulta feita (feita para valores numericos)
+
+min() - retorna o menor valor entre uma tabela ou referente a consulta feita (feita para valores numericos)
+
+avg() - retorna a media dos valores de uma tabela ou referente a consulta feita (feita para valores numericos)
+
+sum() - retorna o somatorio dos valores de uma tabela ou referente a consulta feita (feita para valores numericos)
+
+ceil() - retorna o menor valor inteiro maior ou igual com base no valor informado (retira a parte decimal pra cima) ex: 7.5=8
+
+floor() - retorna o menor valor inteiro menor ou igual com base no valor informado (retira a parte decimal) ex: 7.5=7
+
+truncate() - retira a parte decimal ex: 7.4=7
+
+round() - arredonda de acordo com o valor informado ex: 7.6=8
+
+
+*/
