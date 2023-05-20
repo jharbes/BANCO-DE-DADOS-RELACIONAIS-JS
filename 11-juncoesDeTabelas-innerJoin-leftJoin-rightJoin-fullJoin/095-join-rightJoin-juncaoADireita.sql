13305e50fa603bfe9ed8a2b78fa51a00c0cae88a
Jorge Harbes <jharbes@hotmail.com>
2023-05-20 14:34:05 -0300

    Create 095-join-rightJoin-juncaoADireita.sql

---
+++ 11-juncoesDeTabelas-innerJoin-leftJoin-rightJoin-fullJoin/095-join-rightJoin-juncaoADireita.sql
@@ -0,0 +1,24 @@
+use universidade_u;
+
+select * from curso;
+select * from disciplina;
+
+select 
+	*
+from
+    curso as c left join disciplina as d on (c.idcurso = d.fk_idcurso);
+    
+select 
+	*
+from
+    curso as c right join disciplina as d on (c.idcurso = d.fk_idcurso);
+    
+select 
+	*
+from
+    disciplina as c left join curso as d on (c.idcurso = d.fk_idcurso);
+    
+select
+    *
+from
+    telefone as t right join aluno as a on (t.fk_idaluno = a.idaluno);
