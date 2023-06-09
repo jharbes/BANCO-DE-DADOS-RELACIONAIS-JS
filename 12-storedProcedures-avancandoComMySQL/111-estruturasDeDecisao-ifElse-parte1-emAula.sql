bfd5ed2b0b865761120c5e0053ed83d1dc67672a
Jorge Harbes <jharbes@hotmail.com>
2023-06-09 18:06:20 -0300

    Update 111-estruturasDeDecisao-ifElse-parte1-emAula.sql

--- 12-storedProcedures-avancandoComMySQL/111-estruturasDeDecisao-ifElse-parte1-emAula.sql
+++ 12-storedProcedures-avancandoComMySQL/111-estruturasDeDecisao-ifElse-parte1-emAula.sql
@@ -57,26 +57,58 @@ delimiter $$
 create procedure proc_ifelse2()
 comment 'Exemplo de uso de condicionais ifelse numero 2'
 begin
-
-	declare idade int default 15;
-    declare sexo char(1) default 'M';
     
-    if idade>=18 and sexo='M' then
+    declare idade int default 15;
+    
+    if idade>=18 and (select sexo from aluno where idaluno=4)='M' then
 		select 'Ele é maior de idade';
 	end if;
     
-    if idade<18 and sexo='M' then
+    if idade<18 and (select sexo from aluno where idaluno=4)='M' then
 		select 'Ele é menor de idade';
 	end if;
     
-    if idade>=18 and sexo='F' then
+    if idade>=18 and (select sexo from aluno where idaluno=4)='F' then
 		select 'Ela é maior de idade';
 	end if;
     
-    if idade<18 and sexo='F' then
+    if idade<18 and (select sexo from aluno where idaluno=4)='F' then
 		select 'Ela é menor de idade';
 	end if;
     
 end
 $$
 delimiter ;
+
+select * from aluno;
+
+drop procedure universidade_u.proc_ifelse2;
+
+call proc_ifelse2();
+
+
+
+delimiter $$
+create procedure proc_ifelse3()
+comment 'Exemplo de uso de condicionais ifelse numero 3'
+begin
+    
+    declare sexo char(1);
+    
+    set sexo=(select sexo from aluno where idaluno=3);
+    
+    if sexo='M' then
+		select 'Ele é homem';
+	else
+		select 'Ela é mulher';
+	end if;
+    
+end
+$$
+delimiter ;
+
+select sexo from aluno where idaluno=3;
+
+drop procedure universidade_u.proc_ifelse3;
+
+call proc_ifelse3();
