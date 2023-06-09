bb22617afc58287ca0b69c978e9502ab4f658dee
Jorge Harbes <jharbes@hotmail.com>
2023-06-09 12:11:59 -0300

    Create 104-variaveis-escopos-oQueSao.sql

---
+++ 12-storedProcedures-avancandoComMySQL/104-variaveis-escopos-oQueSao.sql
@@ -0,0 +1,27 @@
+/*
+
+O que são variáveis e escopos?
+
+	- O que são variaveis?
+	
+		Regras para declaração de variaveis
+			
+			- Nao podem ser utilizados caracteres especiais como 'ç', '^', '~'.
+			
+			- Não podem ser iniciadas com numeros
+			
+			- Não podem ser iguais as palavras reservadas do SGBD
+			
+			
+		Escopo das variaveis
+		
+			- Global
+			
+			- Sessão
+			
+			- Parâmetros
+			
+			- Local
+			
+			
+*/
